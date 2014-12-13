ad01d73840ebc9f1bd24895a4d953f5559e78ef1
dagda1 <dagda1@scotalt.net>
2014-12-13 11:01:37 +0000

    upgrade om

--- project.clj
+++ project.clj
@@ -1,13 +1,12 @@
 (defproject cliplater "0.1.0-SNAPSHOT"
   :description ""
   :url "http://example.com/FIXME"
-  :repositories {"local" ~(str (.toURI (java.io.File. "maven_repository")))}
 
   :dependencies [
                  [org.clojure/clojure "1.6.0"]
                  [org.clojure/clojurescript "0.0-2411"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
-                 [self/om "0.8.0-beta2"]
+                 [om "0.8.0-beta3"]
                  [cljs-http "0.1.16"]
                  [com.cemerick/piggieback "0.1.3"]
                  [weasel "0.4.0-SNAPSHOT"]
